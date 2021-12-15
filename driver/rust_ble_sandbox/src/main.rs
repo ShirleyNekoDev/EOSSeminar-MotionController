f3923337bf09a806a136fec0fa9c2398e5b907cb
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2021-12-15 22:49:36 +0100

    [driver] Add helloworld with rust/btleplug

---
+++ driver/rust_ble_sandbox/src/main.rs
@@ -0,0 +1,40 @@
+use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter};
+use btleplug::platform::{Adapter, Manager};
+use std::error::Error;
+use std::time::Duration;
+use tokio::time;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await.unwrap();
+
+    // get the first bluetooth adapter
+    let adapters = manager.adapters().await?;
+    let central = adapters.into_iter().nth(0).unwrap();
+
+    // start scanning for devices
+    central.start_scan(ScanFilter::default()).await?;
+    // instead of waiting, you can use central.event_receiver() to fetch a channel and
+    // be notified of new devices
+    println!("Scanning for devices...");
+    time::sleep(Duration::from_secs(10)).await;
+
+    // find the device we're interested in
+    detect_devices(&central).await;
+
+    Ok(())
+}
+
+async fn detect_devices(central: &Adapter) {
+    for p in central.peripherals().await.unwrap() {
+        let properties = p.properties().await.unwrap().unwrap();
+        let address = properties.address;
+        let maybe_name = properties.local_name;
+
+        match maybe_name {
+            Some(name) => println!("Found {} ({})", address, name),
+            None => println!("Found {}", address),
+        }
+    }
+}
