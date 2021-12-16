67eac5030df8cbf15a1d0b358b5b43b63e14df0c
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2021-12-16 19:51:56 +0100

    [firmware] Add clang-format

--- firmware/helloworld/hello_world.cpp
+++ firmware/helloworld/hello_world.cpp
@@ -5,20 +5,20 @@
 #include "Arduino.h"
 
 void setup() {
-	// initialize serial communication at 9600 bits per second:
-	Serial.begin(9600);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 
-	// print out hello world
-	Serial.println("Hello World");
+  // print out hello world
+  Serial.println("Hello World");
 
-	// Setup to blink the inbuilt LED
+  // Setup to blink the inbuilt LED
 #ifdef LED_BUILTIN
-	pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 #endif
 }
 
 void loop() {
-	// Blink the inbuilt LED
+  // Blink the inbuilt LED
 #ifdef LED_BUILTIN
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
