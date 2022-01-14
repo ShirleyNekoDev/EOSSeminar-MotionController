b76d5be47794f7b52856091aadd51a74b8fb5bb8
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2022-01-14 19:09:02 +0100

    Begin embedding of websocket server for clients into driver

--- driver/rust_ble_sandbox/src/main.rs
+++ driver/rust_ble_sandbox/src/main.rs
@@ -1,36 +1,110 @@
-use btleplug::api::{Central, Manager as _, Peripheral as _, Characteristic, ScanFilter};
+use btleplug::api::{Central, Manager as _, Peripheral as _, Characteristic, ScanFilter, ValueNotification};
 use btleplug::platform::{Adapter, Manager, Peripheral};
+use tokio_stream::wrappers::IntervalStream;
 use std::error::Error;
 use std::option::Option;
 use std::time::Duration;
 use tokio::time;
 use uuid::Uuid;
 use futures::stream::StreamExt;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{accept_async, WebSocketStream, tungstenite::Error as TError};
+use tungstenite::Message;
+use serde::{Serialize, Deserialize};
+
 
 const DIYMOTIONCONTROLLER_SERVICE_UUID: &str = "328c9225-877f-4189-89a8-b50bb21b02ae";
 
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum ClientCommand {
+    LedSet { r: u8, g: u8, b: u8 },
+    RumbleStart,
+    RumbleStop,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum ClientUpdate {
+
+}
+
+fn on_ws_message(msg: Message, _controller_write_method: fn(Uuid, Vec<u8>)) {
+    if let Message::Binary(data) = msg {
+        match bincode::deserialize::<ClientCommand>(&data).unwrap() {
+            ClientCommand::LedSet { r: _, g: _, b: _ } => {
+
+            },
+            ClientCommand::RumbleStop => {
+
+            },
+            ClientCommand::RumbleStart => {
+
+            },
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let manager = Manager::new().await.unwrap();
+    // let manager = Manager::new().await.unwrap();
 
-    // get the first bluetooth adapter
-    let adapters = manager.adapters().await?;
-    let central = adapters.into_iter().nth(0).unwrap();
+    // // get the first bluetooth adapter
+    // let adapters = manager.adapters().await?;
+    // let central = adapters.into_iter().nth(0).unwrap();
 
-    let controller_peripheral = wait_for_motion_controller(&central).await;
-    let _ = controller_peripheral.connect().await?;
+    // let controller_peripheral = wait_for_motion_controller(&central).await;
+    // let _ = controller_peripheral.connect().await?;
 
-    println!("We are connected :party:");
-    println!("found characteristics:");
+    // println!("We are connected :party:");
+    // println!("found characteristics:");
 
-    for characteristic in controller_peripheral.characteristics().iter() {
-        println!(" - {}", characteristic.uuid.to_string());
-        listen_for_updates(&controller_peripheral, characteristic).await?;
+    let ws_stream = wait_for_client_connection().await?;
+    let (mut _ws_sender, mut ws_receiver) = ws_stream.split();
+
+    // for characteristic in controller_peripheral.characteristics().iter() {
+    //     controller_peripheral.subscribe(characteristic).await?;
+    // }
+
+    // for characteristic in controller_peripheral.characteristics().iter() {
+    //     controller_peripheral.unsubscribe(characteristic).await?;
+    // }
+
+    // let mut notification_stream = controller_peripheral.notifications().await?;
+
+    let interval = tokio::time::interval(Duration::from_secs(1));
+    let interval_stream = IntervalStream::new(interval);
+    let mut notification_stream = interval_stream.map(move |_| {
+        ValueNotification { value: vec![42], uuid: Uuid::nil() }
+    });
+
+    loop {
+        tokio::select! {
+            Some(data) = notification_stream.next() => {
+                let characteristic_uuid = data.uuid;
+                let characteristic_value = data.value;
+                println!("From characteristic {} received value {:02X?}.", characteristic_uuid, characteristic_value);
+            },
+            Some(msg) = ws_receiver.next() => {
+                on_ws_message(msg?, |characteristic_uuid, characteristic_value| {
+                    println!("To characteristic {} sent value {:02X?}.", characteristic_uuid, characteristic_value);
+                });
+            },
+        }
     }
+}
 
-    let _ = controller_peripheral.disconnect().await?;
+async fn wait_for_client_connection() -> Result<WebSocketStream<TcpStream>, Box<dyn Error>> {
+    let addr = "127.0.0.1:9001";
+    let server = TcpListener::bind(&addr).await.expect("Cannot listen on port 9001.");
 
-    Ok(())
+    println!("Waiting for connection on addres {}", addr);
+    let (stream, _) = server.accept().await?;
+    let peer = stream.peer_addr().expect("connected streams should have a peer address");
+    println!("We are connected to peer with address {}", peer);
+    let ws_stream = accept_async(stream).await.expect("Failed to accept websocket.");
+    println!("WebSocket connection successful");
+    return Ok(ws_stream);
 }
 
 async fn listen_for_updates(controller: &Peripheral, characteristic: &Characteristic) -> Result<(), Box<dyn Error>> {
