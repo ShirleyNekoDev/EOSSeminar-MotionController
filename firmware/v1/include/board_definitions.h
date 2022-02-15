6dec62f99e596cfa19db455ba4bb1a682b48a003
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2022-02-16 00:58:30 +0100

    Add board definition for prototype 2

--- firmware/v1/include/board_definitions.h
+++ firmware/v1/include/board_definitions.h
@@ -3,6 +3,8 @@
 
 #if defined(ARDUINO_AVR_NANO)
 #include "boards/avr_nano.h"
+#elif defined(ARDUINO_ESP32C3_DEV) && defined(BOARD_PROTOTYPE_2)
+#include "boards/esp32_esp32c3_prototype2.h"
 #elif defined(ARDUINO_ESP32C3_DEV)
 #include "boards/esp32_esp32c3.h"
 #endif
---
+++ firmware/v1/include/boards/esp32_esp32c3_prototype2.h
@@ -0,0 +1,28 @@
+#ifndef BOARD_ESP32_ESP32C3_PROTOTYPE2_H__
+#define BOARD_ESP32_ESP32C3_PROTOTYPE2_H__
+
+// ------ PINS ------
+#define PIN_I2C_SDA 9
+#define PIN_I2C_SCL 10
+
+#define PIN_JOYSTICK_X 1
+#define PIN_JOYSTICK_Y 2
+
+#define PIN_LED_R 3
+#define PIN_LED_G 4
+#define PIN_LED_B 5
+
+#define PIN_BUTTON_MENU 18
+#define PIN_BUTTON_A 8
+#define PIN_BUTTON_B 7
+
+#define PIN_RUMBLE 6
+
+// ------ HARDWARE_CONSTANTS ------
+#define ADC_BIT_RESOLUTION 12
+#define BOARD_BAUDRATE 115200
+
+#define INVERT_X_AXIS true
+#define INVERT_Y_AXIS false
+
+#endif
